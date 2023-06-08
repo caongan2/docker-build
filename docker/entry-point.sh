546210f407e93e7ad36c3a6bb2c62c99e92e6dbb
ngan_ct <nganct@mor.com.vn>
2023-06-08 14:36:50 +0700

    ...

--- docker/entry-point.sh
+++ docker/entry-point.sh
@@ -1,9 +1,9 @@
 composer update --ignore-platform-reqs
 cp .env.example .env
 php artisan key:generate
-chmod -R 777 public
-chmod -R 777 storage
-php artisan storage:link
-chown -R $USER:www-data storage
-chown -R $USER:www-data bootstrap/cache
-php artisan migrate --seed
+#chmod -R 777 public
+#chmod -R 777 storage
+#php artisan storage:link
+#chown -R $USER:www-data storage
+#chown -R $USER:www-data bootstrap/cache
+#php artisan migrate
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 git pull origin HEAD
 cd docker
-sudo docker-compose up --build -d
+sudo docker-compose build
+sudo docker-compose up -d
