3b7df7dccaec6c690db9ebc1494f41f6078e44fa
ngan_ct <caothuongngan@gmail.com>
2023-06-09 01:36:35 +0700

    commit

--- docker/entry-point.sh
+++ docker/entry-point.sh
@@ -1,9 +1,10 @@
 composer update --ignore-platform-reqs
 cp .env.example .env
 php artisan key:generate
-#chmod -R 777 public
-#chmod -R 777 storage
-#php artisan storage:link
-#chown -R $USER:www-data storage
-#chown -R $USER:www-data bootstrap/cache
-#php artisan migrate
+chmod -R 777 public
+chmod -R 777 storage
+php artisan storage:link
+php artisan optimize
+chown -R $USER:www-data storage
+chown -R $USER:www-data bootstrap/cache
+php artisan migrate
--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 git pull origin HEAD
 cd docker
-sudo docker-compose build
-sudo docker-compose up -d
+docker-compose up --build -d
+cd $(pwd)
+docker exec test_app composer update --ignore-platform-reqs
+docker exec test_app cp .env.example .env
+docker exec test_app php artisan key:generate
+docker exec test_app chmod -R 777 public
+docker exec test_app chmod -R 777 storage
+docker exec test_app php artisan storage:link
+docker exec test_app php artisan optimize
+docker exec test_app chown -R $USER:www-data storage
+docker exec test_app chown -R $USER:www-data bootstrap/cache
+docker exec test_app php artisan migrate
